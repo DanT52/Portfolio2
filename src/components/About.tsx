fe9da5d85f30be3dff5f32a529e2d297c3e4e5a9
DanT52 <dantramp52@gmail.com>
2025-01-14 11:18:55 -0800

    add images to aboutme

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,18 +11,43 @@ export const About = () => {
 
   const images = [
     {
-      src: 'https://images.unsplash.com/photo-1735306005581-e09d7dda15b1?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Enjoying a beautiful sunset hike.',
+        src: 'https://i.imgur.com/A5gjuFa.jpeg',
+        description: '',
     },
     {
-      src: 'https://images.unsplash.com/photo-1735263759278-ed793ef6f1e7?q=80&w=2536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: '',
+      src: 'https://i.imgur.com/q15ImPd.jpeg',
+      description: 'Ski!',
     },
     {
-      src: 'https://i.pinimg.com/originals/9a/3c/3f/9a3c3fb5f73822af8514df07f6676392.gif',
-      description: '',
+      src: 'https://i.imgur.com/WXwEGTW.jpeg',
+      description: 'Flip!',
+    },
+    {
+        src: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2x4bDl1aTFqY2pkc3AwYWFodnd2aHd5dGpkNTlicWhvZHp3a2gwbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rgP5fev9HtxezjPNMc/giphy.gif',
+        description: 'On the rollerblades.',
+    },
+    {
+        src: 'https://i.imgur.com/Ig6OFfm.jpeg',
+        description: 'Beacon Rock!',
+    },
+    {
+        src: 'https://i.imgur.com/3H0ODYE.jpeg',
+        description: 'hike and think',
+    },
+    {
+        src: 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjY0b3d2Y2Zxc2xpY3JhZzNkcXpsaGJiejN4bDBnOGJiNjlzb2F5eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/omSh7OpQ9bKB40Ypqu/giphy.gif',
+        description: 'A little frontflip',
+    },
+    {
+        src: 'https://i.imgur.com/P6thHdH.jpeg',
+        description: 'From a road trip to Montana.',
+    },
+    {
+        src: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZ0dWh1bzgzdGFjdGsxZXR0OGhoODRpb241c3AxeWY2OXRocWd1cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Zc7AKroqjhQn5mDFgp/giphy.gif',
+        description: 'Hawaii water jump.',
     },
   ];
+
 
   return (
     <section id="about" className="py-20 bg-background-50 dark:bg-background-950">
@@ -55,7 +80,7 @@ export const About = () => {
             <div className="lg:w-1/2 w-full order-none lg:order-1">
   <Swiper
     modules={[Autoplay, Navigation]}
-    autoplay={{ delay: 3000 }}
+    autoplay={{ delay: 8000 }}
     navigation
     loop
     className="rounded-lg max-w-[500px] mx-auto" // Added max-width and center
