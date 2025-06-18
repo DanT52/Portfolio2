f16f666437b83c19be0439acc36f2b5f37ad2ed7
Daniel Basarab <dantramp52@gmail.com>
2025-06-18 13:47:24 -0700

    remove about me images from site

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,44 +9,44 @@ import 'swiper/css/autoplay';
 export const About = () => {
   const [ref, controls] = useScrollAnimation();
 
-  const images = [
-    {
-        src: 'https://i.imgur.com/A5gjuFa.jpeg',
-        description: '',
-    },
-    {
-      src: 'https://i.imgur.com/q15ImPd.jpeg',
-      description: 'Ski!',
-    },
-    {
-      src: 'https://i.imgur.com/WXwEGTW.jpeg',
-      description: 'Flip!',
-    },
-    {
-        src: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2x4bDl1aTFqY2pkc3AwYWFodnd2aHd5dGpkNTlicWhvZHp3a2gwbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rgP5fev9HtxezjPNMc/giphy.gif',
-        description: 'On the rollerblades.',
-    },
-    {
-        src: 'https://i.imgur.com/Ig6OFfm.jpeg',
-        description: 'Beacon Rock!',
-    },
-    {
-        src: 'https://i.imgur.com/3H0ODYE.jpeg',
-        description: 'hike and think',
-    },
-    {
-        src: 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjY0b3d2Y2Zxc2xpY3JhZzNkcXpsaGJiejN4bDBnOGJiNjlzb2F5eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/omSh7OpQ9bKB40Ypqu/giphy.gif',
-        description: 'A little frontflip',
-    },
-    {
-        src: 'https://i.imgur.com/P6thHdH.jpeg',
-        description: 'From a road trip to Montana.',
-    },
-    {
-        src: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZ0dWh1bzgzdGFjdGsxZXR0OGhoODRpb241c3AxeWY2OXRocWd1cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Zc7AKroqjhQn5mDFgp/giphy.gif',
-        description: 'Hawaii water jump.',
-    },
-  ];
+  // const images = [
+  //   {
+  //       src: 'https://i.imgur.com/A5gjuFa.jpeg',
+  //       description: '',
+  //   },
+  //   {
+  //     src: 'https://i.imgur.com/q15ImPd.jpeg',
+  //     description: 'Ski!',
+  //   },
+  //   {
+  //     src: 'https://i.imgur.com/WXwEGTW.jpeg',
+  //     description: 'Flip!',
+  //   },
+  //   {
+  //       src: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2x4bDl1aTFqY2pkc3AwYWFodnd2aHd5dGpkNTlicWhvZHp3a2gwbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rgP5fev9HtxezjPNMc/giphy.gif',
+  //       description: 'On the rollerblades.',
+  //   },
+  //   {
+  //       src: 'https://i.imgur.com/Ig6OFfm.jpeg',
+  //       description: 'Beacon Rock!',
+  //   },
+  //   {
+  //       src: 'https://i.imgur.com/3H0ODYE.jpeg',
+  //       description: 'hike and think',
+  //   },
+  //   {
+  //       src: 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjY0b3d2Y2Zxc2xpY3JhZzNkcXpsaGJiejN4bDBnOGJiNjlzb2F5eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/omSh7OpQ9bKB40Ypqu/giphy.gif',
+  //       description: 'A little frontflip',
+  //   },
+  //   {
+  //       src: 'https://i.imgur.com/P6thHdH.jpeg',
+  //       description: 'From a road trip to Montana.',
+  //   },
+  //   {
+  //       src: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZ0dWh1bzgzdGFjdGsxZXR0OGhoODRpb241c3AxeWY2OXRocWd1cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Zc7AKroqjhQn5mDFgp/giphy.gif',
+  //       description: 'Hawaii water jump.',
+  //   },
+  // ];
 
 
   return (
@@ -63,13 +63,13 @@ export const About = () => {
           transition={{ duration: .6 }}
           className="max-w-5xl mx-auto"
         >
-          <div className="flex flex-col-reverse lg:flex-row items-center gap-8">
+          <div className="flex flex-col items-center">
             {/* About Me Text */}
-            <div className="lg:w-1/2 w-full order-1 lg:order-none">
-              <h2 className="text-4xl font-bold mb-8 text-text-900 dark:text-text-50 text-center lg:text-left">
+            <div className="max-w-3xl">
+              <h2 className="text-4xl font-bold mb-8 text-text-900 dark:text-text-50 text-center">
                 About Me
               </h2>
-                <p className="text-lg text-text-900 dark:text-text-200 leading-relaxed">
+                <p className="text-lg text-text-900 dark:text-text-200 leading-relaxed text-left">
                 👋 I'm Daniel, a computer science graduate with an interest in software engineering and fullstack
                 development. I earned my Bachelor's degree from WSUv in 2025. I enjoy working with various technologies
                 to create interactive applications.
@@ -77,8 +77,8 @@ export const About = () => {
             </div>
 
             {/* Swiper Slideshow */}
-            <div className="lg:w-1/2 w-full order-none lg:order-1">
-  <Swiper
+            <div className="w-full hidden">
+  {/* <Swiper
     modules={[Autoplay, Navigation]}
     autoplay={{ delay: 8000 }}
     navigation
@@ -105,7 +105,7 @@ export const About = () => {
         </div>
       </SwiperSlide>
     ))}
-  </Swiper>
+  </Swiper> */}
 </div>
           </div>
         </motion.div>
